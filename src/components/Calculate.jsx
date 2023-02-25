d3dbafe06d004ff736cb69eeb0d46a81da9d6e0c
chirag4851 <chiragchavda1243@gmail.com>
2023-02-25 11:23:34 +0530

    quantity feature

--- src/components/Calculate.jsx
+++ src/components/Calculate.jsx
@@ -1,6 +1,17 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
+// import * as d3 from "d3";
 import { useLocation } from "react-router-dom";
+import "./components.css";
+import { Bar } from "react-chartjs-2";
+import Chart from 'chart.js/auto';
+import {LinearScale} from 'chart.js';
+// import { Line } from "react-chartjs-2";
+
+
+Chart.register(LinearScale);
+
+
 function Calculate() {
   const location = useLocation();
   const bfdata = location?.state?.Breakfastlist;
@@ -12,22 +23,82 @@ function Calculate() {
   const [protien,setprotien] = useState(0)
   const [fat,setfat] = useState(0)
   const [carbs,setcarbs] = useState(0)
-
-useEffect(() => {
-    let proteinSum = 0;
-    let fatSum=0;
-    let carbsSum=0;
-    for(let i=0;i<totaldata.length;i++)
-    {
-        proteinSum += totaldata[i].protein;
-        fatSum+=totaldata[i].fat;
-        carbsSum+=totaldata[i].carbs;
-    }
-    setprotien(proteinSum);
-    setfat(fatSum);
-    setcarbs(carbsSum);
-  }, [totaldata]);
+ 
+  function calculateTotalMacros(data) {
+    let totalProtein = 0;
+    let totalFat = 0;
+    let totalCarbs = 0;
   
+    data.reduce((acc, curr) => {
+      totalProtein += curr.protein * parseInt(curr.quantity);
+      totalFat += curr.fat * parseInt(curr.quantity);
+      totalCarbs += curr.carbs * parseInt(curr.quantity);
+    }, 0);
+  
+    return {
+      protein: totalProtein,
+      fat: totalFat,
+      carbs: totalCarbs,
+    };
+  }
+  
+  const totalmacros = calculateTotalMacros(totaldata)
+  console.log(totalmacros)
+  useEffect(()=>{
+    setprotien(totalmacros.protein)
+    setfat(totalmacros.fat)
+    setcarbs(totalmacros.carbs)
+  },[totalmacros])
+
+
+  const data = {
+    labels: ["Protein", "Fat", "Carbs"],
+    datasets: [
+      {
+        label: "Macro-nutrients",
+        data: [protien, fat, carbs],
+        fill: true,
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+        ],
+        borderWidth: 3,
+      },
+      {
+        label: "required values",
+        data: [60, 100, 250], // fixed values for Protein, Fat and Carbs
+        fill: true,
+        backgroundColor: [
+          "rgba(255, 0, 0, 0.5)",
+          "rgba(255, 0, 0, 0.5)",
+          "rgba(255, 0, 0, 0.5)",
+        ],
+        borderColor: [
+          "rgba(255, 0, 0, 1)",
+          "rgba(255, 0, 0, 1)",
+          "rgba(255, 0, 0, 1)",
+        ],
+        borderWidth: 3,
+      },
+    ],
+  };
+  
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  };
+  
+  
+
 
   return (
     <div>
@@ -40,9 +111,9 @@ useEffect(() => {
               key={index}
             >
               <h3>{item.name}</h3>
-              <p>Protein: {item.protein}</p>
-              <p>Carbs: {item.carbs}</p>
-              <p>Fat: {item.fat}</p>
+              <p>Protein: {item.protein*item.quantity}</p>
+              <p>Carbs: {item.carbs*item.quantity}</p>
+              <p>Fat: {item.fat*item.quantity}</p>
             </div>
           ))}
         </div>
@@ -56,9 +127,9 @@ useEffect(() => {
               key={index}
             >
               <h3>{item.name}</h3>
-              <p>Protein: {item.protein}</p>
-              <p>Carbs: {item.carbs}</p>
-              <p>Fat: {item.fat}</p>
+              <p>Protein: {item.protein*item.quantity}</p>
+              <p>Carbs: {item.carbs*item.quantity}</p>
+              <p>Fat: {item.fat*item.quantity}</p>
             </div>
           ))}
         </div>
@@ -72,9 +143,9 @@ useEffect(() => {
               key={index}
             >
               <h3>{item.name}</h3>
-              <p>Protein: {item.protein}</p>
-              <p>Carbs: {item.carbs}</p>
-              <p>Fat: {item.fat}</p>
+              <p>Protein: {item.protein*item.quantity}</p>
+              <p>Carbs: {item.carbs*item.quantity}</p>
+              <p>Fat: {item.fat*item.quantity}</p>
             </div>
           ))}
         </div>
@@ -84,7 +155,9 @@ useEffect(() => {
         <p>protien:{protien}</p>
         <p>carbs:{carbs}</p>
         <p>fat:{fat}</p>
+        
       </div>
+      <Bar data={data} options={options} />
     </div>
   );
 }
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -5,17 +5,14 @@ import "./components.css";
 import logo from "./utils/logo.png";
 import user from "./utils/Profile.png";
 
-
-
 function LandingPage() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const data = [
-    { name: "alooparatha", protein: 5, carbs: 10, fat: 0 },
-    { name: "bread", protein: 8, carbs: 60, fat: 8 },
-    { name: "tea", protein: 6, carbs: 16, fat: 7 },
-    { name: "apple", protein: 4, carbs: 106, fat: 57 },
+    { name: "alooparatha", protein: 5, carbs: 10, fat: 0 ,quantity:1},
+    { name: "bread", protein: 8, carbs: 60, fat: 8 ,quantity:1},
+    { name: "tea", protein: 6, carbs: 16, fat: 7,quantity:1 },
+    { name: "apple", protein: 4, carbs: 106, fat: 57,quantity:1 },
   ];
-
 
   // hooks
   const [breakfast, setbreakfast] = useState("none");
@@ -25,8 +22,6 @@ function LandingPage() {
   const [Breakfastlist, setBreakfastList] = useState([]);
   const [Lunchlist, setLunchList] = useState([]);
   const [Dinnerlist, setDinnerList] = useState([]);
-
-  
 
   useEffect(() => {
     console.log(Breakfastlist);
@@ -95,31 +90,45 @@ function LandingPage() {
     }
   };
 
+  const deletebfElement = (e) => {
+    // console.log(e)
+    const index = e.target.parentNode.getAttribute("key");
+    const updatedList = [...Breakfastlist];
+    updatedList.splice(Breakfastlist.length - index - 1, 1);
+    setBreakfastList(updatedList);
+  };
 
-
-  const deletebfElement =(e)=>{
-  // console.log(e)
-  const index = e.target.parentNode.getAttribute("key");
-  const updatedList = [...Breakfastlist];
-  updatedList.splice(Breakfastlist.length-index-1, 1);
-  setBreakfastList(updatedList);
-  }
-
-  const deletelhElement =(e)=>{
+  const deletelhElement = (e) => {
     // console.log(e)
     const index = e.target.parentNode.getAttribute("key");
     const updatedList = [...Lunchlist];
-    updatedList.splice(Lunchlist.length-index-1, 1);
+    updatedList.splice(Lunchlist.length - index - 1, 1);
     setLunchList(updatedList);
-    }
+  };
 
-    const deletedrElement =(e)=>{
-      // console.log(e)
-      const index = e.target.parentNode.getAttribute("key");
-      const updatedList = [...Dinnerlist];
-      updatedList.splice(Dinnerlist.length-index-1, 1);
-      setDinnerList(updatedList);
-      }
+  const deletedrElement = (e) => {
+    // console.log(e)
+    const index = e.target.parentNode.getAttribute("key");
+    const updatedList = [...Dinnerlist];
+    updatedList.splice(Dinnerlist.length - index - 1, 1);
+    setDinnerList(updatedList);
+  };
+
+  const updateQuantitybf = (index, value) => {
+    const updatedList = [...Breakfastlist];
+    updatedList[index].quantity = value;
+    setBreakfastList(updatedList);
+  }
+  const updateQuantitylh = (index, value) => {
+    const updatedList = [...Lunchlist];
+    updatedList[index].quantity = value;
+    setLunchList(updatedList);
+  }
+  const updateQuantitydr = (index, value) => {
+    const updatedList = [...Dinnerlist];
+    updatedList[index].quantity = value;
+    setDinnerList(updatedList);
+  }
 
   return (
     <div>
@@ -142,15 +151,64 @@ function LandingPage() {
           <div className="additems">
             <p>breakfast</p>
             <button onClick={() => changeBreakfast()}>+</button>
+            <div>
+              {Breakfastlist.map((item, index) => (
+                <div key={index}>
+                  <h3>{item.name}</h3>
+                  <p>quantity</p>
+                  <p>{item.quantity}</p>
+                  <button onClick={(e) => deletebfElement(e)}>delete</button>
+                  {/* <p>Protein: {item.protein}</p>
+                                  <p>Carbs: {item.carbs}</p>
+                                  <p>Fat: {item.fat}</p> */}
+                </div>
+              ))}
+            </div>
           </div>
           <div className="additems">
             <p>lunch</p>
             <button onClick={() => changeLunch()}>+</button>
+            <div>
+              {Lunchlist.map((item, index) => (
+                <div key={index}>
+                  <h3>{item.name}</h3>
+                  <p>quantity</p>
+                  <p>{item.quantity}</p>
+                  <button onClick={(e) => deletelhElement(e)}>delete</button>
+                  {/* <p>Protein: {item.protein}</p>
+                                  <p>Carbs: {item.carbs}</p>
+                                  <p>Fat: {item.fat}</p> */}
+                </div>
+              ))}
+            </div>
           </div>
           <div className="additems">
             <p>dinner</p>
             <button onClick={() => changeDinner()}>+</button>
+            <div>
+              {Dinnerlist.map((item, index) => (
+                <div key={index}>
+                  <h3>{item.name}</h3>
+                  <p>quantity</p>
+                  <p>{item.quantity}</p>
+                  <button onClick={(e) => deletedrElement(e)}>delete</button>
+                </div>
+              ))}
+            </div>
           </div>
+          <button
+            onClick={() => {
+              navigate("/Calculate", {
+                state: {
+                  Breakfastlist: Breakfastlist,
+                  Lunchlist: Lunchlist,
+                  Dinnerlist: Dinnerlist,
+                },
+              });
+            }}
+          >
+            calculate
+          </button>
         </div>
         <div
           className="Breakfast"
@@ -169,12 +227,17 @@ function LandingPage() {
             <div>
               {Breakfastlist.map((item, index) => (
                 <div key={index}>
-                  <h3>{item.name}</h3>
-                  <button onClick={(e)=>deletebfElement(e)} >delete</button>
-                  {/* <p>Protein: {item.protein}</p>
-                                  <p>Carbs: {item.carbs}</p>
-                                  <p>Fat: {item.fat}</p> */}
+                  <ul>
+                    <li>{item.name}</li>
+                    <p>quantity</p>
+                    <input type="text" onChange={(e) => updateQuantitybf(index, e.target.value)} />
+                  </ul>
                 </div>
+              ))}
+            </div>
+            <div>
+              {data.map((item) => (
+                <p>{item.name}</p>
               ))}
             </div>
           </div>
@@ -196,12 +259,15 @@ function LandingPage() {
             <div>
               {Lunchlist.map((item, index) => (
                 <div key={index}>
-                  <h3>{item.name}</h3>
-                  <button onClick={(e)=>deletelhElement(e)} >delete</button>
-                  {/* <p>Protein: {item.protein}</p>
-                                  <p>Carbs: {item.carbs}</p>
-                                  <p>Fat: {item.fat}</p> */}
+                  <li>{item.name}</li>
+                  <p>quantity</p>
+                    <input type="text" defaultValue='1' onChange={(e) => updateQuantitylh(index, e.target.value)} />
                 </div>
+              ))}
+            </div>
+            <div>
+              {data.map((item) => (
+                <p>{item.name}</p>
               ))}
             </div>
           </div>
@@ -223,28 +289,21 @@ function LandingPage() {
             <div>
               {Dinnerlist.map((item, index) => (
                 <div key={index}>
-                  <h3>{item.name}</h3>
-                  <button onClick={(e)=>deletedrElement(e)}>delete</button>
-                  {/* <p>Protein: {item.protein}</p>
-                                  <p>Carbs: {item.carbs}</p>
-                                  <p>Fat: {item.fat}</p> */}
+                  <li>{item.name}</li>
+                  <p>quantity</p>
+                    <input type="text" defaultValue='1' onChange={(e) => updateQuantitydr(index, e.target.value)} />
                 </div>
+              ))}
+            </div>
+            <div>
+              {data.map((item) => (
+                <p>{item.name}</p>
               ))}
             </div>
           </div>
         </div>
       </div>
-      <div>
-        <button onClick={()=>{
-          navigate("/Calculate", {
-            state: {
-              Breakfastlist: Breakfastlist,
-              Lunchlist:Lunchlist,
-              Dinnerlist:Dinnerlist
-            }
-          })
-        }}>calculate</button>
-      </div>
+      <div></div>
     </div>
   );
 }
