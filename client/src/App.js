3fbc6715fd21ff972b794c9d6cbc099620c0e080
chirag4851 <chiragchavda1243@gmail.com>
2023-03-22 13:29:17 +0530

    adding auth 0

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,26 @@
 // import logo from './logo.svg';
-import './App.css';
-import LandingPage from './components/LandingPage';
-import Calculate from './components/Calculate';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import "./App.css";
+import LandingPage from "./components/LandingPage";
+import Calculate from "./components/Calculate";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<LandingPage/>} />
-        <Route path='/Calculate' element={<Calculate/>}/>
-      </Routes>
-    </BrowserRouter>
+    <Auth0Provider
+      domain="dev-njmaitf8k2yax3vn.us.auth0.com"
+      clientId="LD1pu4LdGmYuyBzR37YcR0fN4NxzcOOX"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    > 
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />{" "}
+          <Route path="/Calculate" element={<Calculate />} />{" "}
+        </Routes>{" "}
+      </BrowserRouter>
+    </Auth0Provider>
   );
 }
 
