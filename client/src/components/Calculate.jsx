7d85b14db3a60b4e9ea4daae4f68c4bc7ab9cdc0
chirag4851 <chiragchavda1243@gmail.com>
2023-03-30 12:31:22 +0530

    autosearch utility

--- client/src/components/Calculate.jsx
+++ client/src/components/Calculate.jsx
@@ -5,6 +5,14 @@ import "./components.css";
 import { Bar } from "react-chartjs-2";
 import Chart from "chart.js/auto";
 import { LinearScale } from "chart.js";
+import {
+  Box,
+  Card,
+  FormLabel,
+  VStack,
+  FormControl,
+  Input,
+} from "@chakra-ui/react";
 
 Chart.register(LinearScale);
 
@@ -14,7 +22,14 @@ function Calculate() {
   const lhdata = location?.state?.Lunchlist;
   const drdata = location?.state?.Dinnerlist;
   const totaldata = bfdata.concat(lhdata, drdata);
-  console.log(totaldata);
+  const [Display, setDisplay] = useState("none");
+  const [Age, setAge] = useState("");
+  const [Weigth, setWeight] = useState("");
+  const [dataOk,setDataok] = useState(false)
+  // const [requiredCarbs, setRequiredCarbs] = useState('');
+  // const [requiredFats, setRequiredFats] = useState('');
+
+  // console.log(totaldata);
 
   const [protien, setprotien] = useState(0);
   const [fat, setfat] = useState(0);
@@ -26,7 +41,7 @@ function Calculate() {
     let totalCarbs = 0;
 
     data.reduce((acc, curr) => {
-      console.log(curr)
+      console.log(curr);
       totalProtein += curr.protien * parseInt(curr.quantity);
       totalFat += curr.fat * parseInt(curr.quantity);
       totalCarbs += curr.carbs * parseInt(curr.quantity);
@@ -42,6 +57,7 @@ function Calculate() {
 
   const totalmacros = calculateTotalMacros(totaldata);
   console.log(totalmacros);
+
   useEffect(() => {
     setprotien(totalmacros.protein);
     setfat(totalmacros.fat);
@@ -69,7 +85,7 @@ function Calculate() {
       },
       {
         label: "required values",
-        data: [60, 100, 250], // fixed values for Protein, Fat and Carbs
+        data: [parseInt(Weigth)*1.5,750,1375], // fixed values for Protein, Fat and Carbs
         fill: true,
         backgroundColor: [
           "rgba(255, 0, 0, 0.5)",
@@ -102,7 +118,11 @@ function Calculate() {
           <div>
             {bfdata.map((item, index) => (
               <div
-                style={{ display: "flex", justifyContent: "space-around" }}
+                style={{
+                  display: "grid",
+                  gridTemplateColumns: "repeat(4, 1fr)",
+                  marginLeft: "10vw",
+                }}
                 key={index}
               >
                 <h3>{item.name}</h3>
@@ -118,7 +138,11 @@ function Calculate() {
           <div>
             {lhdata.map((item, index) => (
               <div
-                style={{ display: "flex", justifyContent: "space-around" }}
+                style={{
+                  display: "grid",
+                  gridTemplateColumns: "repeat(4, 1fr)",
+                  marginLeft: "10vw",
+                }}
                 key={index}
               >
                 <h3>{item.name}</h3>
@@ -134,7 +158,11 @@ function Calculate() {
           <div>
             {drdata.map((item, index) => (
               <div
-                style={{ display: "flex", justifyContent: "space-around" }}
+                style={{
+                  display: "grid",
+                  gridTemplateColumns: "repeat(4, 1fr)",
+                  marginLeft: "10vw",
+                }}
                 key={index}
               >
                 <h3>{item.name}</h3>
@@ -145,8 +173,15 @@ function Calculate() {
             ))}
           </div>
         </div>
-        <div style={{ display: "flex", justifyContent: "space-around" , alignItems:'center',marginTop:'2vh'}}>
-          <h1>Total</h1>
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(4, 1fr)",
+            marginTop: "10px",
+            alignItems: "center",
+          }}
+        >
+          <h1 style={{ marginRight: "21.3vw" }}>Total</h1>
           <p>Protien:{protien}</p>
           <p>Carbs:{carbs}</p>
           <p>Fat:{fat}</p>
@@ -156,7 +191,62 @@ function Calculate() {
           url('https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap');
         </style>
       </div>
-      <Bar data={data} options={options} />
+      {Age && Weigth && dataOk ? (
+        <Bar data={data} options={options} />
+      ) : (
+        <div
+          onMouseOver={() => {
+            setDisplay("block");
+          }}
+          onMouseLeave={() => {
+            setDisplay("none");
+          }}
+          style={{ position: "relative", width: "80vw", marginLeft: "10vw" }}
+        >
+          <div className="grayFilm"></div>
+          <Bar data={data} options={options} />
+          <Box className="InputForm" style={{ display: `${Display}` }}>
+            <VStack>
+              <Card
+                bg="#f6f8fa"
+                variant="outline"
+                borderColor="#d8dee4"
+                maxW="40vw"
+                borderRadius="5px"
+                padding="10px"
+              >
+                <FormControl>
+                  <FormLabel>Age</FormLabel>
+                  <Input
+                    onChange={(e) => {
+                      setAge(e.target.value);
+                    }}
+                    type="text"
+                    bg="white"
+                    borderColor="#d8dee4"
+                    size="md"
+                    borderRadius="6px"
+                    width="25vw"
+                  ></Input>
+                  <FormLabel>Weight</FormLabel>
+                  <Input
+                    onChange={(e) => {
+                      setWeight(e.target.value);
+                    }}
+                    type="text"
+                    bg="white"
+                    borderColor="#d8dee4"
+                    size="md"
+                    borderRadius="6px"
+                    width="25vw"
+                  ></Input>
+                </FormControl>
+                <button onClick={()=>{setDataok(true)}}>Submit</button>
+              </Card>
+            </VStack>
+          </Box>
+        </div>
+      )}
     </div>
   );
 }
